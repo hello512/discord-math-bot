0cfa250a9dffc5c6832518126ae3030889e58297
hello512 <anton.anderten@me.com>
2020-08-23 15:41:30 +0200

    chenged tab size

--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,9 @@ import string
 import src.vectormath
 
 
+MATHBOTINFOEMBED = discord.Embed()
+
+
 def extractpoint(msg):
 	"""This function extracts a point from a givven message in the format: (x, y, z)
 	   it checks everything to make sure, that the message is valid."""
@@ -32,29 +35,33 @@ def extractline(msg):
 
 
 def extractplane():
-	pass
+	if msg.translate({ord(c): None for c in "1234567890,. -*"}) != "x=()+r()+s()":
+		return 
 
 ##	contains all the headers for the diffrent equations and the funciton names that need to be called.
-EQUATIONS_HEADERS= {
+EQUATION_HEADERS= {
 	"G:" : extractline,
 	"P:" : extractpoint,
-	"E:" : extractplane
+	"E:" : extractplane,
+	"info" : lambda : MATHBOTINFOEMBED
+	"help" : lambda : MATHBOTINFOEMBED
 
 }
 
 
-def analysemsg(msg):#
+
+def analysemathmsg(msg):#
 	"""this function analyses the message sennd by the user and calls the function
 	   that extracts the actual values of the message"""
 	classes = []
 	msg = msg.split(" ")
 	for index, m in enumerate(msg):
 		if index % 2 != 0:
-			if msg[index] not in EQUATIONS_HEADERS:
+			if msg[index] not in EQUATION_HEADERS:
 				return "not found"
-			classes.append(EQUATIONS_HEADERS[msg[index]](msg[index + 1]))
+			classes.append(EQUATION_HEADERS[msg[index]](msg[index + 1]))
 
-	##	now comes the code that starts the actual calculations
+	##	now comes the code that starts the calculations
 	returncontent = []
 	print(classes)
 	for cl in classes:
@@ -68,7 +75,12 @@ def analysemsg(msg):#
 
 	return returncontent
 
-
+def makestring(contentlist):
+	##	puts the strings stored in the contentlist into one final string that gets send to the client
+	returnmessage = "result:\n"
+	for elem in contentlist:
+		returnmessage = returnmessage + str(elem) + " "
+	return returnmessage
 
 
 class MathBot(discord.Client):
@@ -80,9 +92,14 @@ class MathBot(discord.Client):
 		if str(message.author) == "test_bot#3640":
 			return
 
+		print(message.channel.name, type(message.channel.name))
+		if message.channel.name != "bot-commands":
+			return
+
 		if message.content.startswith(".math"):
-			bot_message = "result:\n"
-			newmsg = analysemsg(message.content)
-			for s in newmsg:
-				bot_message = bot_message + str(s) + " "
-			await message.channel.send(bot_message)
+			botmessage = analysemathmsg(message.content)
+			await message.channel.send(makestring(botmessage))
+
+		if message.content.startswith(".?") or message.content.startswith(".help"):
+			##	sends an info embed back to the user
+			return 
