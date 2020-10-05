d53ca979defb0ca1157191d54c261393e036cf3a
hello512 <anton.anderten@me.com>
2020-10-05 23:04:15 +0200

    added cogs. Not finished yet. Commited them as a backup

---
+++ cogs/help.py
@@ -0,0 +1,15 @@
+from discord.ctx import commands
+
+
+
+class Helper(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def questionmark(self):
+        return
+
+    @commands.command()
+    async def help(self):
+        return
---
+++ cogs/math.py
@@ -0,0 +1,8 @@
+from discord.ext import commands
+
+class Math(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def math(self)
