d356dc7c7310bcf4db6bfeb48ed8157d1343804f
hello512 <anton.anderten@me.com>
2020-10-10 14:07:59 +0200

    added command not available message

--- messages/text_messages.py
+++ messages/text_messages.py
@@ -1,1 +1,4 @@
 MATHBOT_ERROR_MESSAGE =  '❌Run ``.math info`` or ``.math help`` to learn everything about the math bot'
+
+##  message send if command is not available
+COMMAND_NOT_AVAILABLE_MESSAGE = "This is not a vaild command! Run ```.help`` or ``.?`` to find all valid commands."
