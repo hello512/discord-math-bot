8e00c84545fc30ec666e5a08dff193fcc500ceca
hello512 <anton.anderten@me.com>
2020-10-10 14:13:05 +0200

    changed something on command not available message

--- messages/text_messages.py
+++ messages/text_messages.py
@@ -1,4 +1,4 @@
 MATHBOT_ERROR_MESSAGE =  '❌Run ``.math info`` or ``.math help`` to learn everything about the math bot'
 
 ##  message send if command is not available
-COMMAND_NOT_AVAILABLE_MESSAGE = "This is not a vaild command! Run ```.help`` or ``.?`` to find all valid commands."
+COMMAND_NOT_AVAILABLE_MESSAGE = "❌This is not a vaild command! Run ``.help`` or ``.?`` to find all valid commands."
