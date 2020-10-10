e024be36ee9e585764b236a51948158d0214ca2f
hello512 <anton.anderten@me.com>
2020-10-10 14:14:13 +0200

    now sends error message if user types in a command that is not availabe

--- bot.py
+++ bot.py
@@ -7,14 +7,25 @@ import sys
 
 
 from discord.ext import commands
+sys.path.append("../")
+from messages import COMMAND_NOT_AVAILABLE_MESSAGE
 
 BOT = commands.Bot(command_prefix = ".", help_command = None)
 #BOT.remove_command("help")
 
+async def on_connect():
+	print("connected")
+
 async def on_ready():
     print("math-bot is now online!")
 
+
+async def on_command_error(ctx, error):
+	await ctx.send(COMMAND_NOT_AVAILABLE_MESSAGE)
+
+BOT.add_listener(on_connect)
 BOT.add_listener(on_ready)
+BOT.add_listener(on_command_error)
 BOT.load_extension("cogs.math")
 BOT.load_extension("cogs.help")
 
