f8e0df1a95942e28d6a62dcaf8607a384bfdb8ef
hello512 <anton.anderten@me.com>
2020-10-10 12:39:44 +0200

    added help cog to the bot. removed some development sutff

--- bot.py
+++ bot.py
@@ -11,16 +11,12 @@ from discord.ext import commands
 BOT = commands.Bot(command_prefix = ".", help_command = None)
 #BOT.remove_command("help")
 
-
-@commands.command()
-async def help(ctx, *arg):
-    await ctx.send("help page: nothing")
-
 async def on_ready():
     print("math-bot is now online!")
 
 BOT.add_listener(on_ready)
 BOT.load_extension("cogs.math")
+BOT.load_extension("cogs.help")
 
 if __name__ == "__main__":
 	with open("test_token.dat", "r") as token_file:
