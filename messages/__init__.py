36cab83028454c0f0045f5ad564cf7cc7d656824
hello512 <anton.anderten@me.com>
2020-10-10 14:13:20 +0200

    added command not available message

--- messages/__init__.py
+++ messages/__init__.py
@@ -4,3 +4,4 @@ from .embeds import GENERALINFOEMBED
 from .embeds import make_result_embed
 
 from .text_messages import MATHBOT_ERROR_MESSAGE
+from .text_messages import COMMAND_NOT_AVAILABLE_MESSAGE
