368ea008ad194724ec27db3e19697e4d3a230a57
hello512 <anton.anderten@me.com>
2020-10-10 12:33:49 +0200

    moved to a command bot. made the math cog work

--- bot.py
+++ bot.py
@@ -6,44 +6,10 @@ import time
 import sys
 
 
-"""
-
-class MathBot(discord.Client):
-	async def on_ready(self):
-		print("logged in as bot")
-
-	#if message posted
-	async def on_message(self, message):
-		if str(message.author) == "test_bot#3640":
-			return
-
-		if message.channel.name != "bot-commands":
-			##	The bot only runs in this specified channels
-			return
-
-		if message.content.startswith(".math info") or message.content.startswith(".math help"):
-			##	User requested an info sheet about the mathbot
-			await message.channel.send(embed = embeds.MATHBOTINFOEMBED)
-			return
-
-		if message.content.startswith(".math"):
-			##	The actual math bot is called
-			botmessage = analysemathmsg(message.content)
-			if type(botmessage).__name__ == "str":
-				await message.channel.send(botmessage)
-			if type(botmessage).__name__ == "Embed":
-				await message.channel.send(embed = botmessage)
-			print(type(botmessage).__name__)
-
-		if message.content.startswith(".?") or message.content.startswith(".help"):
-			##	sends an info embed back to the user
-			await message.channel.send(embed = embeds.GENERALINFOEMBED)
-
-"""
 from discord.ext import commands
 
 BOT = commands.Bot(command_prefix = ".", help_command = None)
-BOT.remove_command("help")
+#BOT.remove_command("help")
 
 
 @commands.command()
