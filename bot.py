7e721e229cc732ed2478c8fb9834b6f05a4b04e3
hello512 <anton.anderten@me.com>
2020-10-11 12:52:52 +0200

    fixed bugs in the guild handler

--- bot.py
+++ bot.py
@@ -21,60 +21,71 @@ class GuildHandler(Thread):
 		self.guilds = bot.guilds
 
 	def load_valid_guild_ids(self):
+		#works
 		with open("guild_ids.yml", "r") as guild_file:
 			content = yaml.safe_load(guild_file)
 			return content["valid_ids"]
 
 	def add_valid_guild_id(self, id):
-		if len(str(id)) != 18 or type(id) != "int":
+		##	works!
+		if len(str(id)) != 18 or type(id).__name__ != "int":
 			return False
-		if id in load_baned_guild_ids():
+		if id in self.load_baned_guild_ids():
 			return "baned"
-		with open("guild_ids.yml", "r+") as guild_file:
+		with open("guild_ids.yml", "r") as guild_file:
 			content = yaml.safe_load(guild_file)
+		with open("guild_ids.yml", "w") as guild_file:
 			content["valid_ids"].append(id)
 			yaml.safe_dump(content, guild_file)
 		return True
 
 	def delete_valid_guild_id(self, id):
-		with open("guild_ids.yml", "r+") as guild_file:
+		#works
+		if len(str(id)) != 18 or type(id).__name__ != "int":
+			return False
+		with open("guild_ids.yml", "r") as guild_file:
 			content = yaml.safe_load(guild_file)
 			try:
-				content["vaild_ids"].remove(id)
-			except:
+				content["valid_ids"].remove(id)
+			except Exception as e:
 				return
+		with open("guild_ids.yml", "w") as guild_file:
 			yaml.safe_dump(content, guild_file)
 
 	def load_baned_guild_ids(self):
+		#works
 		with open("guild_ids.yml", "r") as guild_file:
 			content = yaml.safe_load(guild_file)
 			return content["baned_ids"]
 		return
 
 	def ban_guild_id(self, id):
-		if len(str(id)) != 18 or type(id) != "int":
+		#works
+		if len(str(id)) != 18 or type(id).__name__ != "int":
 			return False
-		with open("guild_ids.yml", "r+") as guild_file:
+		with open("guild_ids.yml", "r") as guild_file:
 			content = yaml.safe_load(guild_file)
 			try:
 				content["valid_ids"].remove(id)
 			except:
 				pass
 			content["baned_ids"].append(id)
+		with open("guild_ids.yml", "w") as guild_file:
 			yaml.safe_dump(content, guild_file)
 
 
 	def unban_guild_id(self, id):
-		if len(str(id)) != 18 or type(id) != "int":
+		#works
+		if len(str(id)) != 18 or type(id).__name__ != "int":
 			return False
-		with open("guild_ids.yml", "r+") as guild_file:
-			content = yaml.safe_load(guild_file)
-			self.delete_valid_guild_id(id)
+		with open("guild_ids.yml", "r") as guild_file:
+			content = yaml.safe_load(guild_file)#
+			try:
+				content["baned_ids"].remove(id)
+			except:
+				return
+		with open("guild_ids.yml", "w") as guild_file:
 			yaml.safe_dump(content, guild_file)
-
-	def check_joined_guild(self, guild):
-		if guild.id not in self.load_valid_guild_ids() or guild.id in load_baned_guild_ids():
-			guild.leave()
 
 
 	def run(self):
