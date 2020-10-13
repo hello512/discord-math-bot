238bfce9670aa7b22832ba7590c4e2e7637e00be
hello512 <anton.anderten@me.com>
2020-10-13 18:31:37 +0200

    not tested. Still developing

--- cogs/guilds.py
+++ cogs/guilds.py
@@ -1,16 +1,21 @@
 from discord.ext import commands, tasks
+import yaml
+
+
+def channel_type(ctx):
+	print(str(ctx.channel.type))
+	return str(ctx.channel.type) == "private"
 
 class GuildHandler(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		print("init")
-		self.handler.start()
+		self.run.start()
 
 	def load_valid_guild_ids(self):
 		#works
 		with open("guild_ids.yml", "r") as guild_file:
 			content = yaml.safe_load(guild_file)
-			return content["valid_ids"]
+			return content["valid_ids"] if content["valid_ids"] != None else []
 
 	def add_valid_guild_id(self, id):
 		##	works!
@@ -42,7 +47,7 @@ class GuildHandler(commands.Cog):
 		#works
 		with open("guild_ids.yml", "r") as guild_file:
 			content = yaml.safe_load(guild_file)
-			return content["baned_ids"]
+			return content["baned_ids"] if content["baned_ids"] != None else []
 		return
 
 	def ban_guild_id(self, id):
@@ -75,15 +80,68 @@ class GuildHandler(commands.Cog):
 
 
 	@tasks.loop(seconds = 0.5, count = None)
-	async def handler(self):
+	async def run(self):
 		##	loops through the guilds the bot is in and leaves the guilds that are not in the valid guild list
 		for guild in self.bot.guilds:
-			print(guild.id, "valid: ", self.load_valid_guild_ids(), " baned:", self.load_baned_guild_ids())
-			if guild.id not in self.load_valid_guild_ids() or guild.id in load_baned_guild_ids():
+			if guild.id not in self.load_valid_guild_ids() or guild.id in self.load_baned_guild_ids():
 				print("leaving guild with id: ", guild.id)
 				await guild.leave()
 
-    ## commands:
+
+	def make_str(self, content):
+		finished_str = "\n".join(content)
+		return "```" + finished_str + "```"
+
+	##	commands:
+
+	@commands.command(name = "ban-guild")
+	@commands.is_owner()
+	@commands.check(channel_type)
+	async def _ban_guild(self, ctx, *args):
+		returncontent = []
+		for id in args:
+			res = self.ban_guild_id(id)
+			returncontent.append(f"{id}: invalid format" if not res else f"{id}: baned")
+		await ctx.send(self.make_str(returncontent))
+
+	@commands.command(name = "unban-guild")
+	@commands.is_owner()
+	@commands.check(channel_type)
+	async def _unban_guild(self, ctx, *args):
+		returncontent = []
+		for id in args:
+			res = self.unban_guild_id(id)
+			returncontent.append(f"{id}: invalid format" if not res else f"{id}: unbaned")
+		await ctx.send(self.make_str(returncontent))
+
+	@commands.command(name = "add-guild")
+	@commands.is_owner()
+	@commands.check(channel_type)
+	async def _add_guild(self, ctx, *args):
+		async def _unban_guild(self, ctx, *args):
+			returncontent = []
+			for id in args:
+				res = self.add_valid_guild_id(id)
+				returncontent.append(f"{id}: invalid format" if not res else f"{id}: added to valid guild ids")
+			await ctx.send(self.make_str(returncontent))
+
+	@commands.command(name = "add-guild")
+	@commands.is_owner()
+	@commands.check(channel_type)
+	async def _remove_guild(self, ctx, *args):
+		async def _unban_guild(self, ctx, *args):
+			returncontent = []
+			for id in args:
+				res = self.delete_valid_guild_id(id)
+				returncontent.append(f"{id}: invalid format" if not res else f"{id}: removed from valid guild ids")
+			await ctx.send(self.make_str(returncontent))
+
+	@commands.command(name = "guild-status")
+	@commands.is_owner()
+	@commands.check(channel_type)
+	async def _guild_status(self, ctx, *args):
+		pass
+
 
 def setup(bot):
-    pass
+    bot.add_cog(GuildHandler(bot))
