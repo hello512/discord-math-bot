c375b209d2d8db82959fdb9ed2c0cb48bd7187b4
hello512 <anton.anderten@me.com>
2020-11-09 19:45:30 +0100

    token is now saved in env var

--- token_parser.py
+++
@@ -1,7 +0,0 @@
-from sys import argv
-
-TOKEN_FILE_PATH = "permanent_storage/test_token.dat"
-
-if __name__ == "__main__":
-    with open(TOKEN_FILE_PATH, "w") as token_file:
-        token_file.write(argv[1])
